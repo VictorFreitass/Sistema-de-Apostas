1a9fa99ef36d16266c4ee0c1f2112c6830182403
VICTOR FREITAS SILVA <victor.fsilva45@gmail.com>
2022-04-04 10:12:39 -0300

    Sistema de Apostas

--- Sistema de Apostas/Program.cs
+++ Sistema de Apostas/Program.cs
@@ -9,39 +9,39 @@ namespace Sistema_de_Apostas
     internal class Program
     {
         static void Main(string[] args)
-        {            
+        {
             Console.WriteLine();
             Console.WriteLine("Digite a quantidade de cartões");
             int c = int.Parse(Console.ReadLine());
             Console.WriteLine("Digite a quantidade de número para cartões");
-            int n = int.Parse(Console.ReadLine());                   
+            int n = int.Parse(Console.ReadLine());
 
             Console.WriteLine();
             int[,] m = new int[c, n];
             Random random = new Random();
             for (int i = 0; i < m.Length; i++)
             {
-              int sorteado = random.Next(1, 60);
-                 if (i == 0)
-                  m[c , n] = sorteado;
-                 else
-                 {
-                   if (Array.BinarySearch(m, sorteado) < 0)
-                   m[c , n] = sorteado;
-                 }                
-                   if (i != m.Length - 1)
-                   Console.WriteLine(m[c , n] + "-");
-                   else
-                   Console.WriteLine(m[c , n]);
+                int sorteado = random.Next(1, 60);
+                if (i == 0)
+                    m[c, n] = sorteado;
+                else
+                {
+                    if (Array.BinarySearch(m, sorteado) < 0)
+                        m[c, n] = sorteado;
+                }
+                if (i != m.Length - 1)
+                    Console.WriteLine(m[c, n] + "-");
+                else
+                    Console.WriteLine(m[c, n]);
             }
             Console.WriteLine();
             Array.Sort(m);
-           for (int i = 0; i < m.Length; i++)
+            for (int i = 0; i < m.Length; i++)
             {
                 if (i != m.Length - 1)
                     Console.Write(m[c, n]);
                 else
-                    Console.Write(m[c ,n]);
+                    Console.Write(m[c, n]);
             }
             Console.ReadKey();
         }
