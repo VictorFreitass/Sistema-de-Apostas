f085bba15f9a7974f69d66118353b07de1a28642
VICTOR FREITAS SILVA <victor.fsilva45@gmail.com>
2022-04-04 08:54:04 -0300

    Sistema de Apostas

--- Sistema de Apostas/Program.cs
+++ Sistema de Apostas/Program.cs
@@ -9,7 +9,14 @@ namespace Sistema_de_Apostas
     internal class Program
     {
         static void Main(string[] args)
-        {
+        {            
+            Console.WriteLine();
+            Console.WriteLine("Digite a quantidade de cartões");
+            string c = Console.ReadLine();
+            Console.WriteLine("Digite a quantidade de número para cartões");
+            int n = int.Parse(Console.ReadLine());
+            int[,] m = new int[c, n];
+
         }
     }
 }
