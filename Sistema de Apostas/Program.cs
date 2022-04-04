e8215875e48bde7b1cd4862bb662e80c02dd02cc
VICTOR FREITAS SILVA <victor.fsilva45@gmail.com>
2022-04-04 10:10:43 -0300

    Sistema de Apostas

--- Sistema de Apostas/Program.cs
+++ Sistema de Apostas/Program.cs
@@ -12,11 +12,38 @@ namespace Sistema_de_Apostas
         {            
             Console.WriteLine();
             Console.WriteLine("Digite a quantidade de cartões");
-            string c = Console.ReadLine();
+            int c = int.Parse(Console.ReadLine());
             Console.WriteLine("Digite a quantidade de número para cartões");
-            int n = int.Parse(Console.ReadLine());
-            int[,] m = new int[c, n];
+            int n = int.Parse(Console.ReadLine());                   
 
+            Console.WriteLine();
+            int[,] m = new int[c, n];
+            Random random = new Random();
+            for (int i = 0; i < m.Length; i++)
+            {
+              int sorteado = random.Next(1, 60);
+                 if (i == 0)
+                  m[c , n] = sorteado;
+                 else
+                 {
+                   if (Array.BinarySearch(m, sorteado) < 0)
+                   m[c , n] = sorteado;
+                 }                
+                   if (i != m.Length - 1)
+                   Console.WriteLine(m[c , n] + "-");
+                   else
+                   Console.WriteLine(m[c , n]);
+            }
+            Console.WriteLine();
+            Array.Sort(m);
+           for (int i = 0; i < m.Length; i++)
+            {
+                if (i != m.Length - 1)
+                    Console.Write(m[c, n]);
+                else
+                    Console.Write(m[c ,n]);
+            }
+            Console.ReadKey();
         }
     }
 }
